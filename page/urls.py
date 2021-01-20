daf181b3fb0923b7ddb65b3066f341a239703448
parkjimin <tjmewellspent@khu.ac.kr>
2021-01-21 00:18:51 +0900

    아이디어페이지

--- page/urls.py
+++ page/urls.py
@@ -8,4 +8,13 @@ urlpatterns = [
     path("idea/create/", view=views.idea_create, name="idea_create"),
     path("idea/update/<int:pk>/", view=views.idea_update, name="idea_update"),
     path("idea/<int:pk>/delete/", view=views.idea_delete, name="idea_delete"),
+    
+    path("devtool/", view=views.devtool_list, name="devtool_list"),
+    path("devtool/<int:pk>/", view=views.devtool_detail, name="devtool_detail"),
+    path("devtool/create/", view=views.devtool_create, name="devtool_create"),
+    path("devtool/update/<int:pk>/",
+         view=views.devtool_update, name="devtool_update"),    
+    path("devtool/<int:pk>/delete/",
+         view=views.devtool_delete, name="devtool_delete"),
 ]
+
--- page/views.py
+++ page/views.py
@@ -60,3 +60,62 @@ def idea_delete(request, pk):
         return redirect("page:idea_list")
     else:
         return redirect("page:idea_detail", pk=idea.pk)
+
+
+
+def devtool_list(request):
+    devtools = Devtool.objects.all()
+    ctx = {
+        "devtools": devtools
+    }
+    return render(request, "page/devtool_list.html", ctx)
+
+
+def devtool_detail(request, pk):
+    devtool = get_object_or_404(Devtool, pk=pk)
+    devtool_ideas = devtool.idea.all()
+    ctx = {
+        "devtool": devtool,
+        "devtool_ideas": devtool_ideas,
+    }
+    return render(request, "page/devtool_detail.html", ctx)
+
+
+def devtool_create(request):
+    if request.method == "POST":
+        form = DevtoolForm(request.POST)
+        if form.is_valid():
+            devtool = form.save()
+            return redirect("page:devtool_detail", pk=devtool.pk)
+    else:
+        form = DevtoolForm()
+        ctx = {
+            "form": form
+        }
+        return render(request, "page/devtool_create.html", ctx)
+
+
+def devtool_update(request, pk):
+    devtool = get_object_or_404(Devtool, pk=pk)
+
+    if request.method == "POST":
+        form = DevtoolForm(request.POST, instance=devtool)
+        if form.is_valid():
+            devtool = form.save()
+            return redirect("page:devtool_detail", pk=devtool.pk)
+    else:
+        form = DevtoolForm(instance=devtool)
+        ctx = {
+            "form": form
+        }
+        return render(request, "page/devtool_update.html", ctx)
+
+
+def devtool_delete(request, pk):
+    devtool = get_object_or_404(Devtool, pk=pk)
+
+    if request.method == "POST":
+        devtool.delete()
+        return redirect("page:devtool_list")
+    else:
+        return redirect("page:devtool_detail", pk=devtool.pk)
